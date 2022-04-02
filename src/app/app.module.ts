0bda315b4015a46149005c81d67137e07c14db42
ridham111 <ridhampansuriya810@gmail.com>
2022-04-02 18:51:11 +0530

    city card and designing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,7 @@ import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pa
 import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { CardEmptyComponent } from './components/cards/card-empty/card-empty.component';
 
 @NgModule({
   declarations: [
@@ -84,6 +85,7 @@ import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user
     IndexComponent,
     LandingComponent,
     ProfileComponent,
+    CardEmptyComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
